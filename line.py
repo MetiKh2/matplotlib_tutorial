41aa490aa7603c41d3cfe6960d0934d83be8dfa7
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2022-12-10 11:18:02 +0330

    Create line.py

---
+++ line.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+y1=np.array([3,8,1,10])
+x1=np.array([0,1,2,3])
+y2=np.array([6,2,7,11])
+x2=np.array([0,1,2,3])
+
+plt.plot(x1,y1,x2,y2)
+# plt.plot(y2)
+plt.show()
