22899b87fe2ebc7ea0f61085ec600b229399a896
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2022-12-11 13:11:43 +0330

    Create scatter.py

---
+++ scatter.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+# colors=np.array(["red","green","blue","yellow","pink","black","orange","purple","beige","brown","gray","cyan","magenta"])
+# colors = np.array([0, 10, 20, 30, 40, 45, 50, 55, 60, 70, 80, 90, 100])
+# sizes = np.array([20,50,100,200,500,1000,60,90,10,300,600,800,75])
+# x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+# y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+x=np.random.randint(100,size=(100))
+y=np.random.randint(100,size=(100))
+colors=np.random.randint(100,size=(100))
+sizes=10*np.random.randint(100,size=(100))
+plt.scatter(x, y,cmap='nipy_spectral',s=sizes,c=colors,alpha=0.5)
+plt.colorbar()
+#day two , the age and speed of 15 cars
+# x = np.array([2,2,8,1,15,8,12,9,7,3,11,4,7,14,12])
+# y = np.array([100,105,84,105,90,99,90,95,94,100,79,112,91,80,85])
+# plt.scatter(x,y,c='#88c999')
+plt.show()
